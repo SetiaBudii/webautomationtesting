fa54dab33caf18e865bc662c943c91f43336a754
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-06-19 13:52:57 +0700

    update driver manager

--- src/test/java/io/cucumber/swaglabs/utils/DriverManager.java
+++ src/test/java/io/cucumber/swaglabs/utils/DriverManager.java
@@ -1,24 +1,58 @@
+// package io.cucumber.swaglabs.utils;
+
+// import org.openqa.selenium.WebDriver;
+// import org.openqa.selenium.chrome.ChromeDriver;
+
+// public class DriverManager {
+//   private static WebDriver driver;
+
+//   public static WebDriver getDriver() 
+//   {
+//     if (driver == null) {
+//       driver = new ChromeDriver();
+//     }
+//     return driver;
+//   }
+
+//   public static void closeDriver() 
+//   {
+//     if (driver != null) {
+//       driver.quit();
+//       driver = null;
+//     }
+//   }
+// }
 package io.cucumber.swaglabs.utils;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverManager {
-  private static WebDriver driver;
+    private static WebDriver driver;
+    private static final String SELENIUM_GRID_URL = "http://103.63.25.132:4444/wd/hub"; // Change to your Selenium Grid URL
 
-  public static WebDriver getDriver() 
-  {
-    if (driver == null) {
-      driver = new ChromeDriver();
+    public static WebDriver getDriver() {
+        if (driver == null) {
+            try {
+                // Use ChromeOptions instead of DesiredCapabilities
+                ChromeOptions options = new ChromeOptions();
+                driver = new RemoteWebDriver(new URL(SELENIUM_GRID_URL), options);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Failed to create WebDriver", e);
+            }
+        }
+        return driver;
     }
-    return driver;
-  }
 
-  public static void closeDriver() 
-  {
-    if (driver != null) {
-      driver.quit();
-      driver = null;
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
     }
-  }
 }
