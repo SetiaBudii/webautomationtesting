782635ac5a2d5730d03dea40a20d78f41af49556
YayangSB <yayang.setia.tif420@polban.ac.id>
2024-07-03 08:26:39 +0700

    try

--- src/test/java/io/cucumber/swaglabs/steps/Hooks.java
+++ src/test/java/io/cucumber/swaglabs/steps/Hooks.java
@@ -7,6 +7,8 @@ import io.cucumber.java.Scenario;
 import io.cucumber.swaglabs.utils.DriverManager;
 import io.cucumber.swaglabs.utils.ScreenshotUtil;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Hooks {
     public static WebDriver driver;
@@ -19,8 +21,13 @@ public class Hooks {
     @After
     public void tearDown(Scenario scenario) {
         if (scenario.isFailed()) {
-            String screenshotName = scenario.getName().replaceAll(" ", "_") + ".png";
+            // Get current timestamp
+            String timestamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            // Create the screenshot name with the timestamp
+            String screenshotName = scenario.getName().replaceAll(" ", "_") + "_" + timestamp + ".png";
+            // Generate the file path
             String filePath = Paths.get(System.getProperty("user.dir"), "screenshots", screenshotName).toString();
+            // Take the screenshot
             ScreenshotUtil.takeScreenshot(driver, filePath);
         }
         DriverManager.closeDriver();
